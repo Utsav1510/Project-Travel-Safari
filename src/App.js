b1bcb01e805ac10c30c813ddb522931ba2309b59
[Siddharth Singh] <siddharth1397.be20@chitkara.edu.in>
2022-09-06 10:21:35 +0530

    Project-Travel-Safari

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div className='middle'>
       <Header />
       <Reviews/>
-      [[</div>
+      </div>
       <Footer />
     </>
   )
--- src/content/Reviews.js
+++ src/content/Reviews.js
@@ -2,61 +2,32 @@ import React from 'react'
 
 export default function Reviews() {
   return (
-<div className="row">
-  <div className="col-4">
-    <nav id="navbar-example3" className="h-100 flex-column align-items-stretch pe-4 border-end">
-      <nav className="nav nav-pills flex-column">
-        <a className="nav-link" href="#review-1">review 1</a>
-        <nav className="nav nav-pills flex-column">
-          <a className="nav-link ms-3 my-1" href="#review-1-1">review 2</a>
-          {/* <a className="nav-link ms-3 my-1" href="#review-1-2">review 3</a> */}
-        </nav>
-        <a className="nav-link" href="#review-2">review 3</a>
-        {/* <a className="nav-link" href="#review-3">review 5</a> */}
-        <nav className="nav nav-pills flex-column">
-          <a className="nav-link ms-3 my-1" href="#review-3-1">review 4</a>
-          {/* <a className="nav-link ms-3 my-1" href="#review-3-2">review 7</a> */}
-        </nav>
-      </nav>
-    </nav>
-  </div>
-
-  <div className="col-8">
-    <div data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-smooth-scroll="true" className="scrollspy-example-2" tabindex="0">
-      <div id="review-1">
-        <h4>review 1</h4>
-        <p>...</p>
+    <>
+    <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
+    <div className="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img src="..." className="d-block w-100" alt="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"/>
       </div>
-      {/* <div id="review-1-1">
-        <h5>review 1-1</h5>
-        <p>...</p>
-      </div> */}
-      {/* <div id="review-1-2">
-        <h5>review 1-2</h5>
-        <p>...</p>
-      </div> */}
-      <div id="review-2">
-        <h4>review 2</h4>
-        <p>...</p>
+      <div className="carousel-item">
+        <img src="..." className="d-block w-100" alt="bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"/>
       </div>
-      <div id="review-3">
-        <h4>review 3</h4>
-        <p>...</p>
-      </div>
-      {/* <div id="review-3-1">
-        <h5>review 3-1</h5>
-        <p>...</p>
-      </div>
-      <div id="review-3-2">
-        <h5>review 3-2</h5>
-        <p>...</p>
-      </div> */}
-        <div id="review-4">
-        <h4>review 4</h4>
-        <p>...</p>
+      <div className="carousel-item">
+        <img src="..." className="d-block w-100" alt="69969696969696696996696969669696"/>
       </div>
     </div>
-  </div>
-</div>
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+  </div></>
  )
 }
